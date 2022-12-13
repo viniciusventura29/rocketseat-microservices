0703b2b628f7e7df8f421d25e0c261e43ca67784
viniciusventura29 <viniciusventura29@gmail.com>
2022-12-13 20:44:23 -0300

    📦 NEW: Connecting prisma bd with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { httpModule } from './http.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [httpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
