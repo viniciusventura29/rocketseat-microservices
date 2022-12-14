eae87297b6fb9da8711f286c3efd614ebc92458b
viniciusventura29 <viniciusventura29@gmail.com>
2022-12-14 16:51:18 -0300

    👌 IMPROVE: Auto date CreatedAt

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,29 +1,49 @@
-export interface NotificationProps{
-    content : string
-    category: string
+import { Replace } from '../helpers/Replace';
+import { Content } from './content';
+
+export interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null | undefined;
+  createdAt: Date;
 }
 
-export class Notification{
-    private props: NotificationProps
+export class Notification {
+  private props: NotificationProps;
 
-    constructor(props: NotificationProps){
-        this.props = props
-    }
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
 
-    public set content(content: string){
-        this.props.content =  content
-    }
+  public set content(content: Content) {
+    this.props.content = content;
+  }
 
-    public set category(category: string){
-        this.props.category =  category
-    }
+  public get content(): Content {
+    return this.props.content;
+  }
 
-    public get content():string{
-        return this.props.content;
-    }
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
 }
-
-const notify = new Notification({
-    content: "Oi dante",
-    category: "Tcahu dante"
-});
